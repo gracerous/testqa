c44b9ebe76ea842c3646273d763fa350beda9554
gracerous <89662012+gracerous@users.noreply.github.com>
2021-09-07 20:56:46 +0300

    Add files via upload

--- test/test1.js
+++ test/test1.js
@@ -2,20 +2,21 @@ const assert = require('assert');
 const fs = require('fs');
 const request = require('request');
 
-let download = function(uri, filename, callback){ 
-    request.head(uri, function(err, res, body){ 
-      console.log('content-type:', res.headers['content-type']); 
-      console.log('content-length:', res.headers['content-length']); 
-   
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback); 
-    }); 
-  };
+describe('Test #1', function () {
+  it('Compare images base on base64', function () {
+    var download = function (uri, filename, callback) {
+      request.head(uri, function (err, res, body) {
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
 
-describe('Test #1', function() {
-    it('Compare images base on base64', function () {
-        download('http://apimeme.com/meme?meme=Alarm-Clock&top=Top+text&bottom=Bottom+text', './compare.jpeg', function () { console.log('done') });
-        let etalon = fs.readFileSync('./test/example.jpg', { encoding: 'base64' });
-        let compared = fs.readFileSync('compare.jpeg', { encoding: 'base64' });
-        assert.equal(etalon, compared);
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+      });
+    };
+    download('http://apimeme.com/meme?meme=Alarm-Clock&top=Top+text&bottom=Bottom+text', 'compare.jpeg', function () {
+      var etalon = fs.readFileSync('./test/example.jpg', { encoding: 'base64' });
+      var compared = fs.readFileSync('compare.jpeg', { encoding: 'base64' });
+      assert.equal(etalon, compared);
     });
+
+  });
 });
